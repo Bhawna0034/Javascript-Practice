20caa7321066694b5c1e8b67998599cec5fa92ad
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-02-04 17:32:53 +0530

    ForEach: Sum of all elements of an array using forEach()

--- LogicBuildingPractice/forEach.js
+++ LogicBuildingPractice/forEach.js
@@ -1,4 +1,19 @@
 let arr = [1,2,3,4,5];
+// arr.forEach(function(val){
+//     console.log("Hello World " + val);
+// });
+
+//Print only those values of array which is less and equal to 2
+// arr.forEach(function(val){
+//     if(val <= 2){
+//         console.log(val);
+//     }
+// });
+
+//Sum of all the element of an array
+let sum=0;
 arr.forEach(function(val){
-    console.log("Hello World " + val);
+   sum+=val;
 })
+console.log("Sum: " + sum);
+
